6b65b720d59e2d651fe58be38273801cc19b1893
Teddy Carpin <tedcarpi@c2r4p5.42perpignan.fr>
2024-11-07 09:51:36 +0100

    libft

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tedcarpi <tedcarpi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 09:04:40 by tedcarpi          #+#    #+#             */
+/*   Updated: 2024/11/07 09:51:17 by tedcarpi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *src)
+{
+	char	*res;
+	int		i;
+
+	res = (char *) malloc(sizeof(char) * (ft_strlen(src) + 1));
+	if (res == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		res[i] = src[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
